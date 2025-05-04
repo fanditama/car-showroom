eb1b9217dc5e8f229ad64b4c294938e69f523b51
Affandi Pratama <afandip911@gmail.com>
2025-05-04 13:27:30 +0700

    Tambah factory untuk Order ID

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -16,7 +16,12 @@ class TransactionFactory extends Factory
      */
     public function definition(): array
     {
+        $orderPrefix = 'ORDER-' . now()->format('Ym');
+        $orderNumber = $this->faker->unique()->numberBetween(10000, 99999);
+        $orderId = $orderPrefix . $orderNumber;
+        
         return [
+            'order_id' => $orderId,
             'transaction_date' => now()->format('d-m-Y H:i:s'),
             'total_amount' => $this->faker->randomFloat(2, 10000, 100000),
             'payment_method' => $this->faker->randomElement(['transfer_bank', 'credit_card', 'cash']),
@@ -26,6 +31,69 @@ class TransactionFactory extends Factory
             'order_address' => $this->faker->address,
             'user_id' => \App\Models\User::factory(),
             'car_id' => \App\Models\Car::factory(),
+            'payment_url' => $this->faker->url,
+            'snap_token' => $this->faker->uuid,
         ];
+    }
+    
+    /**
+     * Set the transaction to pending status
+     */
+    public function pending(): self
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'status' => 'pending',
+            ];
+        });
+    }
+    
+    /**
+     * Set the transaction to processing status
+     */
+    public function processing(): self
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'status' => 'processing',
+            ];
+        });
+    }
+    
+    /**
+     * Set the transaction to success status
+     */
+    public function success(): self
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'status' => 'success',
+                'payment_date' => now()->format('d-m-Y H:i:s'),
+            ];
+        });
+    }
+    
+    /**
+     * Set the transaction to cancel status
+     */
+    public function cancel(): self
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'status' => 'cancel',
+            ];
+        });
+    }
+    
+    /**
+     * Set the transaction to failed status
+     */
+    public function failed(): self
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'status' => 'failed',
+            ];
+        });
     }
 }
